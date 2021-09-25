af00f2f03d1680d90dad611b0f1e4901433ed1a2
Anas AbuGhalieh <civil.Anas1000@gmail.com>
2021-09-25 13:53:15 +0300

    petApp : add pet view permission

--- pet/views.py
+++ pet/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
 from .models import Pet
 from .serializers import PetSerializer
-
+from .permissions import IsOwnerOrReadOnly
 
 class PetList(generics.ListCreateAPIView):
 
@@ -10,7 +10,6 @@ class PetList(generics.ListCreateAPIView):
 
 
 class PetDetail(generics.RetrieveDestroyAPIView):
-
+    permission_classes = (IsOwnerOrReadOnly,)
     queryset = Pet.objects.all()
     serializer_class = PetSerializer
-
