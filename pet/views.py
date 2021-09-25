75de99da0f4a6a79c9b7728ad4823596c1332603
AbdullahELian1 <abodelian28@gmail.com>
2021-09-25 14:45:34 +0300

    supplise Feature :add list view and detaile view for supplise

--- pet/views.py
+++ pet/views.py
@@ -1,7 +1,9 @@
 from rest_framework import generics
-from .models import Pet
-from .serializers import PetSerializer
+from .models import Pet,Supplies
+from .serializers import PetSerializer,SuppliesSerializer
 from .permissions import IsOwnerOrReadOnly
+from rest_framework.permissions import IsAdminUser
+
 
 class PetList(generics.ListCreateAPIView):
 
@@ -14,4 +16,14 @@ class PetDetail(generics.RetrieveDestroyAPIView):
     queryset = Pet.objects.all()
     serializer_class = PetSerializer
 
+class SuppliesList(generics.ListCreateAPIView):
+    permission_classes = (IsAdminUser,)
+    queryset = Supplies.objects.all()
+    serializer_class = SuppliesSerializer
 
+
+class SuppliesDetail(generics.RetrieveDestroyAPIView):
+    permission_classes = (IsAdminUser,)
+
+    queryset = Supplies.objects.all()
+    serializer_class = SuppliesSerializer
