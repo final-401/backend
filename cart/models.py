56966c7149dad87ca8702e269a6234bbb60fa57f
AbdullahELian1 <abodelian28@gmail.com>
2021-09-25 22:53:40 +0300

    Cart feature: add order  model

--- cart/models.py
+++ cart/models.py
@@ -15,4 +15,19 @@ class OrderItem(models.Model):
         return self.product.product_name
 
 
+class Order(models.Model):
+    ref_code = models.CharField(max_length=15)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
+    is_ordered = models.BooleanField(default=False)
+    items = models.ManyToManyField(OrderItem)
+    date_ordered = models.DateTimeField(auto_now=True)
+
+    def get_cart_items(self):
+        return self.items.all()
+
+    def get_cart_total(self):
+        return sum([item.product.price for item in self.items.all()])
+
+    def __str__(self):
+        return '{0} - {1}'.format(self.owner, self.ref_code)
 
