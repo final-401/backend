2a0ee72cfb8cc12d2851d7ee26884722108b1433
Anas AbuGhalieh <civil.Anas1000@gmail.com>
2021-09-27 23:02:34 +0300

    Write Tests for Pet Model

--- pet/urls.py
+++ pet/urls.py
@@ -5,6 +5,6 @@ from .views import PetList, PetDetail,SuppliesList,SuppliesDetail
 urlpatterns = [
     path('pet/<int:pk>/', PetDetail.as_view(), name='Petdetail'),
     path('pet/', PetList.as_view(), name='Petlist'),
-    path('supplies/<int:pk>/', SuppliesDetail.as_view(), name='Petdetail'),
-    path('supplies/', SuppliesList.as_view(), name='Petlist'),
+    path('supplies/<int:pk>/', SuppliesDetail.as_view(), name='SuppliesDetail'),
+    path('supplies/', SuppliesList.as_view(), name='SuppliesList'),
 ]
